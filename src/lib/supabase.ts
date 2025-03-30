69ec7166420aa086e42ad1c3edc0230f1a49808d
muthu-sathuram <muthukumar@sathuramsoft.com>
2025-03-30 22:00:39 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // These should be environment variables in a production app
-const supabaseUrl = 'https://your.supabase.co';
-const supabaseAnonKey = '';
+const supabaseUrl = 'https://ipvlgmduyvvqucizpgpm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwdmxnbWR1eXZ2cXVjaXpwZ3BtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzMzQzNTMsImV4cCI6MjA1ODkxMDM1M30.ZnJ00flmZOU6OT7o1QObUuRlj1EFLcDgxlIhsFjBOs4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
